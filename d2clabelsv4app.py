80c9ecf4ad55df84351711073c8aadd3291ff308
JuanBelisario <juan.belisario@trafilea.com>
2024-09-25 18:34:42 -0300

    Update d2clabelsv4app.py

--- d2clabelsv4app.py
+++ d2clabelsv4app.py
@@ -5,7 +5,7 @@ import re
 from io import BytesIO
 from reportlab.lib.pagesizes import mm
 from reportlab.pdfgen import canvas
-from barcode import EAN13, Code128
+from barcode import Code128
 from barcode.writer import ImageWriter
 from datetime import datetime
 from zipfile import ZipFile
@@ -126,94 +126,14 @@ def generate_fnsku_labels_from_excel(df):
 
         progress_bar.progress((index + 1) / total_rows)
 
-    # Comprimir los PDFs generados en un archivo ZIP
+    # Comprimir solo los PDFs que tengan el sufijo "_fnsku_label" en el nombre
     zip_filename = f"{output_folder}.zip"
     with ZipFile(zip_filename, 'w') as zipObj:
         for folder_name, subfolders, filenames in os.walk(output_folder):
             for filename in filenames:
-                filepath = os.path.join(folder_name, filename)
-                zipObj.write(filepath, os.path.basename(filepath))
-
-    return zip_filename
-
-# Función para generar etiquetas D2C en PDF con UPC
-def generate_label_pdf(sku, upc_code, lot_num, output_path):
-    width, height = 60 * mm, 35 * mm
-    c = canvas.Canvas(output_path, pagesize=(width, height))
-    x_margin = 4.5 * mm
-    y_sku = height - 7.75 * mm
-    y_barcode = height / 2 - 8 * mm
-    y_lot = 4.75 * mm
-    barcode_width = 51.5 * mm
-    c.setFont("Helvetica", 9.5)
-    c.drawCentredString(width / 2, y_sku, sku)
-    if len(upc_code) == 12:
-        upc_code = '0' + upc_code
-    barcode_filename = clean_filename(f"{sku}_barcode")
-    barcode_path = f"{barcode_filename}.png"
-    options = {
-        'module_width': 0.35,
-        'module_height': 16,
-        'font_size': 7.75,
-        'text_distance': 4.5,
-        'quiet_zone': 1.25,
-        'dpi': 600
-    }
-    barcode_ean = EAN13(upc_code, writer=ImageWriter())
-    barcode_ean.save(barcode_filename, options)
-    c.drawImage(barcode_path, (width - barcode_width) / 2, y_barcode, width=barcode_width, height=16 * mm)
-    os.remove(barcode_path)
-    c.setFont("Helvetica", 9)
-    if lot_num:
-        lot_box_width = 40 * mm
-        lot_box_height = 4 * mm
-        x_lot_box = (width - lot_box_width) / 2
-        y_lot_box = y_lot - 1.125 * mm
-        c.setStrokeColorRGB(0, 0, 0)
-        c.rect(x_lot_box, y_lot_box, lot_box_width, lot_box_height, stroke=1, fill=0)
-        c.drawCentredString(width / 2, y_lot, lot_num)
-    c.save()
-
-# Función para la generación de PDFs D2C y FNSKU
-def generate_pdfs_from_excel(df, label_type="D2C"):
-    required_columns = ['SKU', 'UPC Code', 'LOT#'] if label_type == "D2C" else ['SKU', 'FNSKU', 'Product Name', 'LOT#']
-    missing_columns = [col for col in required_columns if col not in df.columns]
-    if missing_columns:
-        st.error(f"Missing columns in the Excel file: {', '.join(missing_columns)}")
-        return None
-
-    first_sku = df.iloc[0]['SKU']
-    current_date = datetime.now().strftime("%Y%m%d")
-
-    output_folder = f"{first_sku}_{current_date}"
-    os.makedirs(output_folder, exist_ok=True)
-
-    total_rows = len(df)
-    progress_bar = st.progress(0)
-
-    for index, row in df.iterrows():
-        sku = str(row['SKU']) if pd.notna(row['SKU']) else ''
-        if label_type == "D2C":
-            upc_code = str(row['UPC Code']).zfill(12)
-            lot_num = str(row['LOT#']) if pd.notna(row['LOT#']) else ""
-            pdf_filename = clean_filename(f"{sku}.pdf")
-            pdf_path = os.path.join(output_folder, pdf_filename)
-            generate_label_pdf(sku, upc_code, lot_num, pdf_path)
-        elif label_type == "FNSKU":
-            fnsku = str(row['FNSKU']) if pd.notna(row['FNSKU']) else ''
-            product_name = str(row['Product Name']) if pd.notna(row['Product Name']) else ''
-            lot_num = str(row['LOT#']) if pd.notna(row['LOT#']) else ""
-            barcode_image = generate_fnsku_barcode(fnsku, sku)
-            create_fnsku_pdf(barcode_image, fnsku, sku, product_name, lot_num, output_folder)
-
-        progress_bar.progress((index + 1) / total_rows)
-
-    zip_filename = f"{output_folder}.zip"
-    with ZipFile(zip_filename, 'w') as zipObj:
-        for folder_name, subfolders, filenames in os.walk(output_folder):
-            for filename in filenames:
-                filepath = os.path.join(folder_name, filename)
-                zipObj.write(filepath, os.path.basename(filepath))
+                if "_fnsku_label" in filename:  # Solo incluir los archivos correctos
+                    filepath = os.path.join(folder_name, filename)
+                    zipObj.write(filepath, os.path.basename(filepath))
 
     return zip_filename
 
